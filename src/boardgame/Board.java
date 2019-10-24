69eb97d0e668155844d9bc2841f7872f67c5a6f9
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-10-24 16:17:58 -0300

    Moving pieces
    Signed-off-by: Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException("Error creating board: there must be at least 1 row and 1 column ");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column.");
 		}
 
 		this.rows = rows;
@@ -49,6 +49,21 @@ public class Board {
 		piece.position = position;
 	}
 
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board.");
+		}
+
+		if (piece(position) == null) {
+			return null;
+		}
+
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -61,7 +76,6 @@ public class Board {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board.");
 		}
-
 		return piece(position) != null;
 	}
 
