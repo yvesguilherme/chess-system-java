afa2f3b3c24426015e34f1d44bcaa9ba98668358
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-10-25 10:00:24 -0300

    Possible moves of a piece.
    Signed-off-by: Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,7 +26,7 @@ public class Program {
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			} catch (ChessException cE) {
 				System.out.println(cE.getMessage());
