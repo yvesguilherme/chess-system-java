8c7ab13ba6744866b793e2cd195b46a9e516bfa2
Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>
2019-10-29 16:56:05 -0300

    Piece move count
    Signed-off-by: Yves Guilherme Lopo Ribeiro Lima <yveslopo@gmail.com>

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount; // padrão inteiro inicia com 0
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,11 +18,23 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
