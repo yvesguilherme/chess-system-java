4df5559e7667c148b531291005343ad6fdea6953
Yves Guilherme <yveslopo@gmail.com>
2019-11-04 11:56:01 -0300

    Queen (and Pawn bugfix)

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -57,7 +57,7 @@ public class Pawn extends ChessPiece {
 
 			// Verifica movimento possível para a primeira jogada, duas casas à frente
 			p.setValues(position.getRow() + 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
 					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
